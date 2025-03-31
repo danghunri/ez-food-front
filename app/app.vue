48b7d089a4a689032774d7662e5a4f9eea2cdb6a
danghunri <sanghoon0120@gmail.com>
2025-03-31 21:16:09 +0900

    init

---
+++ app/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <v-app>
+      <VContainer>
+        <v-btn variant="elevated">asd</v-btn>
+        <VBtn color="primary">asd</VBtn>
+        <v-btn @click="toggleTheme">toggle theme</v-btn>
+      </VContainer>
+    </v-app>
+  </div>
+</template>
+
+<script setup>
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+export default defineNuxtConfig({
+  _modules: ["@nuxt/eslint"],
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
